cad17042040666935e80b7a7d34aa90ec244c002
fudeel <fadil_mucia@hotmail.com>
2020-11-18 19:40:47 +0100

    created homepage module with lazy loading routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+  {
+    path: 'homepage',
+    loadChildren: () => import('./screens/homepage/homepage.module').then(m => m.HomepageModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
