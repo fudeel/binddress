c167bdcaa3cc52971fb6c506ac7f941e81c19954
fudeel <fadil_mucia@hotmail.com>
2020-11-19 19:19:28 +0100

    created loading spinner

--- src/app/screens/locale/homepage.ts
+++ src/app/screens/locale/homepage.ts
@@ -1,13 +1,14 @@
 const HOMEPAGE_LOCALE = {
-        mainText: [
-            'Bind it to you, NOW!', 'Bindalo a te, ORA!'
-        ],
-        subtitle: [
-            'Register an article and bind it to you', 'Registra un articolo e legalo a te'
-        ],
-        search: ['Search for bound items', 'Cerca articoli bindati'],
-        searchButton : ['Search', 'Cerca']
-    }
+    mainText: [
+      'Bind it to you, NOW!', 'Bindalo a te, ORA!'
+    ],
+    subtitle: [
+      'Register an article and bind it to you', 'Registra un articolo e legalo a te'
+    ],
+    search: ['Search for bound items', 'Cerca articoli bindati'],
+    searchButton: ['Search', 'Cerca'],
+    searchPlaceholder: ['Identifier', 'Codice identificatore']
+  }
 ;
 
 export default HOMEPAGE_LOCALE;
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatIconModule } from '@angular/material/icon'
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
 
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { CarouselComponent } from './components/carousel/carousel.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {ToolbarComponent} from './components/toolbar/toolbar.component';
+import {CarouselComponent} from './components/carousel/carousel.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDialogModule} from '@angular/material/dialog';
-import { DialogComponent } from './components/dialog/dialog.component';
-
-
+import {DialogComponent} from './components/dialog/dialog.component';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 
 @NgModule({
@@ -27,7 +26,8 @@ import { DialogComponent } from './components/dialog/dialog.component';
     ReactiveFormsModule,
     MatInputModule,
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatProgressBarModule
   ],
   exports: [
     MatToolbarModule,
@@ -40,7 +40,8 @@ import { DialogComponent } from './components/dialog/dialog.component';
     MatInputModule,
     MatButtonModule,
     MatDialogModule,
-    DialogComponent
+    DialogComponent,
+    MatProgressBarModule
   ]
 })
 export class SharedModule { }
